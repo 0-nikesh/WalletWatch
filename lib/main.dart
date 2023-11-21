4c5f189500b1f37a6d3fe031ebc8c026a8a1fafd
Nikesh <bhandarinikesh93@gmail.com>
2023-11-21 22:59:49 +0545

    color sorted accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  //color defination
+  static const Color textColor = Color.fromRGBO(223, 246, 255, 100);
+  static const Color bgColor = Color.fromRGBO(6, 40, 61, 100);
+  static const Color btnColor = Color.fromRGBO(71, 181, 255, 100);
+  static const Color logoColor = Color.fromRGBO(37, 109, 133, 100);
 
   @override
   Widget build(BuildContext context) {
