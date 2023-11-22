fd68fbcd799ac8223ae482ef909e764a18bcd965
Nikesh <bhandarinikesh93@gmail.com>
2023-11-22 22:15:19 +0545

    edited some gestures

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:walletwatch/components/btn.dart';
 import 'package:walletwatch/components/textfield.dart';
 import 'package:walletwatch/main.dart';
+import 'package:walletwatch/pages/register_page.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+  final Function()? onTap;
+  LoginPage({super.key, required this.onTap});
 
   //text editing controller
   final usernameController = TextEditingController();
@@ -16,138 +18,146 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(6, 40, 61, 100),
-        body: SafeArea(
+      backgroundColor:
+          Colors.transparent, // Make the scaffold background transparent
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF000000), // Start color (#47B5FF)
+              Color(0xFF06283D), // End color (#06283D)
+            ],
+          ),
+        ),
+        child: SafeArea(
           child: SingleChildScrollView(
             child: Center(
-              child: Column(children: [
-                //logo
-                SizedBox(
-                  height: 80,
-                ),
-
-                Image.asset(
-                  "lib/image/normal.png",
-                  height: 120,
-                  width: 120,
-                ),
-
-                SizedBox(
-                  height: 50,
-                ),
-
-                //welcome back
-                Text(
-                  "Control, Track, Grow, With WalletWatch",
-                  style: TextStyle(
-                      color: Color.fromRGBO(223, 246, 255, 100), fontSize: 16),
-                ),
-                SizedBox(height: 50),
-
-                //username
-                MyTextField(
-                  controller: usernameController,
-                  hintText: "Username",
-                  obscureText: false,
-                ),
-
-                SizedBox(
-                  height: 25,
-                ),
-
-                //password
-                MyTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  obscureText: true,
-                ),
-                SizedBox(height: 20),
-
-                //forgot password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Image.asset(
+                    "lib/image/normal.png",
+                    height: 120,
+                    width: 120,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    "Control, Track, Grow, With WalletWatch",
+                    style: TextStyle(
+                      color: Color.fromRGBO(223, 246, 255, 100),
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: "Username",
+                    obscureText: false,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: "Password",
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Forgot Password ?",
+                          style: TextStyle(color: MyApp.textColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  MyBtn(
+                    onTap: signIn,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        "Forgot Password ?",
-                        style: TextStyle(color: MyApp.textColor),
+                        "Haven't Signed Up?",
+                        style: TextStyle(color: MyApp.textColor, fontSize: 15),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          " Register Now",
+                          style: TextStyle(color: MyApp.btnColor, fontSize: 18),
+                        ),
                       ),
                     ],
                   ),
-                ),
-
-                SizedBox(height: 20),
-
-                //sign in button
-
-                MyBtn(
-                  onTap: signIn,
-                ),
-
-                SizedBox(
-                  height: 20,
-                ),
-
-                //register now
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Haven't Signed Up?",
-                      style: TextStyle(color: MyApp.textColor, fontSize: 15),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.3,
+                            color: MyApp.textColor,
+                          ),
+                        ),
+                        Text(
+                          "Or Continue with",
+                          style: TextStyle(color: MyApp.textColor),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.3,
+                            color: MyApp.textColor,
+                          ),
+                        )
+                      ],
                     ),
-                    Text(
-                      " Register Now",
-                      style: TextStyle(color: MyApp.btnColor, fontSize: 18),
-                    ),
-                  ],
-                ),
-
-                SizedBox(
-                  height: 30,
-                ),
-
-                //or continue with
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Row(
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.3,
-                          color: MyApp.textColor,
-                        ),
+                      Image.asset(
+                        "lib/image/image8-2.webp",
+                        height: 100,
+                        width: 120,
                       ),
-                      Text(
-                        "Or Continue with",
-                        style: TextStyle(color: MyApp.textColor),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.3,
-                          color: MyApp.textColor,
-                        ),
-                      )
                     ],
                   ),
-                ),
-
-                SizedBox(
-                  height: 20,
-                ),
-                //google
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("lib/image/image8-2.webp",
-                        height: 100, width: 120)
-                  ],
-                )
-              ]),
+                ],
+              ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
