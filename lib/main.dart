7e0a998baf63b57ddcc3cfa3d5b37cbb24009995
Nikesh <bhandarinikesh93@gmail.com>
2024-02-25 21:39:38 +0545

    current user

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:walletwatch/firebase_options.dart';
-import 'package:walletwatch/pages/auth_page.dart';
+import 'package:walletwatch/pages/login_page.dart';
 import 'package:walletwatch/pages/receipts_page.dart';
 import 'package:walletwatch/pages/statistics_page.dart';
 import 'package:walletwatch/pages/transaction_page.dart';
@@ -15,18 +16,22 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  static User? currentUser;
+
   const MyApp({super.key});
 
   static const Color textColor = Color.fromRGBO(223, 246, 255, 100);
   static const Color bgColor = Color.fromRGBO(6, 40, 61, 100);
-  static const Color btnColor = Color.fromRGBO(71, 181, 255, 100);
+  static const Color btnColor = Color.fromARGB(156, 71, 181, 255);
   static const Color logoColor = Color.fromRGBO(37, 109, 133, 100);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AuthPage(),
+      home: LoginPage(
+        onTap: () {},
+      ),
       routes: {
         '/statistics': (context) => StatisticPage(),
         '/transaction': (context) => TransactionPage(),
@@ -35,3 +40,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//trying trying
